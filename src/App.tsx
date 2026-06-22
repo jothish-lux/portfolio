4970c3d2425d5084b6ff29e0c487358dd0d4e7af
Jo <jarupar1@stu.vistula.edu.pl>
2026-06-22 16:11:13 +0000

    added smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './router';
+import { useEffect } from 'react';
+import Lenis from 'lenis';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <BrowserRouter basename={__BASE_PATH__}>
       <AppRoutes />
